2e2f04b95432ca6be9096da84911bd7c06247aab
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-08-11 15:24:27 +0800

    Update doc comment.

--- src/Sudoku.Analytics/Analytics/StepSearchers/StepSearcherRuntimeFlags.cs
+++ src/Sudoku.Analytics/Analytics/StepSearchers/StepSearcherRuntimeFlags.cs
@@ -12,7 +12,8 @@ namespace Sudoku.Analytics.StepSearchers;
 public enum StepSearcherRuntimeFlags
 {
 	/// <summary>
-	/// Indicates the step searcher can be called anywhere as long it is enabled. This is also the default value of this enumeration type.
+	/// Indicates the step searcher can be called anywhere as long it is enabled.
+	/// This is also the default value of this enumeration type.
 	/// </summary>
 	Default = 0,
 
@@ -29,8 +30,20 @@ public enum StepSearcherRuntimeFlags
 	IndirectTechniquesOnly = 1 << 3,
 
 	/// <summary>
+	/// <para>
 	/// Indicates conclusions produced by the current <see cref="StepSearcher"/> can be skipped to be verified,
 	/// because they can be guaranteed as correct ones.
+	/// </para>
+	/// <para>
+	/// <see cref="StepSearcher"/> configuring this flag will skip verifying validity of conclusions found.
+	/// If there's any conclusion is invalid, it will cause a severe problem,
+	/// e.g. a runtime crash or <see cref="WrongStepException"/>.
+	/// </para>
+	/// <para>
+	/// Please think over and then use this flag.
+	/// Don't use this flag if a step searcher produces unknown conclusions of validity.
+	/// </para>
 	/// </summary>
+	/// <seealso cref="WrongStepException"/>
 	SkipVerification = 1 << 4
 }
