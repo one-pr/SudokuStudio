a6fe34f4fde94adbcf3eea83194b8584c98c18cf
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-08-16 20:53:24 +0800

    Update code.

--- src/System/GenericExtensions.cs
+++ src/System/GenericExtensions.cs
@@ -6,6 +6,28 @@ namespace System;
 public static class GenericExtensions
 {
 	/// <summary>
+	/// Provides extension members on <see cref="object"/>?.
+	/// </summary>
+	extension<T>(T) where T : class
+	{
+#if EXTENSION_OPERATORS
+		/// <summary>
+		/// Determines whether the current value is non-<see langword="null"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>A <see cref="bool"/> result indicating that.</returns>
+		public static bool operator true(T? value) => value is not null;
+
+		/// <summary>
+		/// Determines whether the current value is <see langword="null"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>A <see cref="bool"/> result indicating that.</returns>
+		public static bool operator false(T? value) => value is null;
+#endif
+	}
+
+	/// <summary>
 	/// Provides extension members on <typeparamref name="T"/>,
 	/// where <typeparamref name="T"/> satisfies <see langword="struct"/> constraint.
 	/// </summary>
