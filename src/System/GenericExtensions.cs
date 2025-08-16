90f41dce9bb930083c8b30a37a5f4a3d4e1e5ce8
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-08-17 00:32:03 +0800

    Update code.

--- src/System/GenericExtensions.cs
+++ src/System/GenericExtensions.cs
@@ -6,10 +6,10 @@ namespace System;
 public static class GenericExtensions
 {
 	/// <summary>
-	/// Provides extension members on <typeparamref name="T"/>,
-	/// where <typeparamref name="T"/> satisfies <see langword="class"/> constraint.
+	/// Provides extension members on <typeparamref name="T"/>?,
+	/// where <typeparamref name="T"/> satisfies <see langword="class"/>? constraint.
 	/// </summary>
-	extension<T>(T) where T : class
+	extension<T>(T?) where T : class?
 	{
 #if EXTENSION_OPERATORS
 		/// <summary>
@@ -33,6 +33,37 @@ public static class GenericExtensions
 		/// <param name="value">The value.</param>
 		/// <returns>A <see cref="bool"/> result indicating that.</returns>
 		public static bool operator false([MaybeNullWhen(true)] T? value) => value is null;
+#endif
+	}
+
+	/// <summary>
+	/// Provides extension members on <typeparamref name="T"/>? (i.e. <see cref="Nullable{T}"/>),
+	/// where <typeparamref name="T"/> satisfies <see langword="struct"/> constraint.
+	/// </summary>
+	extension<T>(T?) where T : struct
+	{
+#if EXTENSION_OPERATORS
+		/// <summary>
+		/// Determines whether the current value is <see langword="null"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>A <see cref="bool"/> result indicating that.</returns>
+		[OverloadResolutionPriority(-1)]
+		public static bool operator !([MaybeNullWhen(true)] T? value) => !value.HasValue;
+
+		/// <summary>
+		/// Determines whether the current value is non-<see langword="null"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>A <see cref="bool"/> result indicating that.</returns>
+		public static bool operator true([NotNullWhen(true)] T? value) => value.HasValue;
+
+		/// <summary>
+		/// Determines whether the current value is <see langword="null"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <returns>A <see cref="bool"/> result indicating that.</returns>
+		public static bool operator false([MaybeNullWhen(true)] T? value) => !value.HasValue;
 #endif
 	}
 
