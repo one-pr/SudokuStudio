e4f5f3034f3d34209e32607a999a23d46bbeea7a
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-09-02 20:13:04 +0800

    Fix a little bug.

--- src/Sudoku.Analytics/Generating/GeneratorHub.optimizers.cs
+++ src/Sudoku.Analytics/Generating/GeneratorHub.optimizers.cs
@@ -3,24 +3,27 @@ namespace Sudoku.Generating;
 public partial class GeneratorHub
 {
 	private static partial Grid DefaultGenerator(Cell givens, SymmetricType symmetry, ConstraintCollection constraints, CancellationToken ct)
-	{
-		var puzzle = new Generator().Generate(givens, symmetry, ct);
-		return puzzle.IsUndefined ? throw new OperationCanceledException() : puzzle;
-	}
+		=> new Generator().Generate(givens, symmetry, ct);
 
 	private static partial Grid Optimizer_FullHouseOnly(Cell givens, SymmetricType type, ConstraintCollection constraints, CancellationToken ct)
-		=> new FullHouseGenerator
+	{
+		new FullHouseGenerator
 		{
 			SymmetricType = type,
 			EmptyCellsCount = givens == -1 ? -1 : 81 - givens
-		}.TryGenerateUnique(out var p, ct) ? p : throw new OperationCanceledException();
+		}.TryGenerateUnique(out var p, ct);
+		return p;
+	}
 
 	private static partial Grid Optimizer_NakedSingleOnly(Cell givens, SymmetricType type, ConstraintCollection constraints, CancellationToken ct)
-		=> new NakedSingleGenerator
+	{
+		new NakedSingleGenerator
 		{
 			SymmetricType = type,
 			EmptyCellsCount = givens == -1 ? -1 : 81 - givens
-		}.TryGenerateUnique(out var p, ct) ? p : throw new OperationCanceledException();
+		}.TryGenerateUnique(out var p, ct);
+		return p;
+	}
 
 	private static partial Grid Optimizer_IttoryuMode(Cell givens, SymmetricType symmetry, ConstraintCollection constraints, CancellationToken ct)
 	{
@@ -31,7 +34,7 @@ public partial class GeneratorHub
 			var puzzle = generator.Generate(givens, symmetry, ct);
 			if (puzzle.IsUndefined)
 			{
-				throw new OperationCanceledException();
+				return puzzle;
 			}
 
 			if (finder.FindPath(puzzle, ct) is { IsComplete: true } path)
@@ -43,22 +46,16 @@ public partial class GeneratorHub
 	}
 
 	private static partial Grid Optimizer_MissingDigit(Cell givens, SymmetricType symmetry, ConstraintCollection constraints, CancellationToken ct)
-	{
 		// Set an arbitrary digit as missing digit.
 		// The digit will be transformed to other one after the puzzle is satisfied the target constraints.
-		var puzzle = new Generator { MissingDigit = 0 }.Generate(givens, symmetry, ct);
-		return puzzle.IsUndefined ? throw new OperationCanceledException() : puzzle;
-	}
+		=> new Generator { MissingDigit = 0 }.Generate(givens, symmetry, ct);
 
 	private static partial Grid Optimizer_EmptyHouses(Cell givens, SymmetricType symmetry, ConstraintCollection constraints, CancellationToken ct)
-	{
-		var puzzle = new EmptyHouseBasedGenerator
+		=> new EmptyHouseBasedGenerator
 		{
 			DesiredMissingHousesMask = constraints.OfType<EmptyHousesCountConstraint>().Aggregate(
 				0,
 				static (interim, next) => interim | (1 << next.Count) - 1 << (int)next.HouseType * 9
 			)
 		}.Generate(ct);
-		return puzzle.IsUndefined ? throw new OperationCanceledException() : puzzle;
-	}
 }
