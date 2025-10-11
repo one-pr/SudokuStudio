5750c7be1efb3b4339ea7b7baf08e84173783ee6
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-10-11 16:39:41 +0800

    Update code.

--- src/Sudoku.Analytics/SetTheory/PatternReasoner.cs
+++ src/Sudoku.Analytics/SetTheory/PatternReasoner.cs
@@ -53,4 +53,30 @@ public static class PatternReasoner
 		}
 		return result.AsSpan();
 	}
+
+	/// <summary>
+	/// Gets all rank-0 links.
+	/// </summary>
+	/// <param name="pattern">The pattern.</param>
+	/// <returns>All rank-0 links.</returns>
+	public static SpaceSet GetRank0Links(in Pattern pattern)
+	{
+		var permutations = GetPermutations(pattern);
+		var result = pattern.Links;
+		foreach (var permutation in permutations)
+		{
+			var lightUpLinks = SpaceSet.Empty;
+			foreach (var candidate in permutation)
+			{
+				var cell = candidate / 9;
+				var digit = candidate % 9;
+				lightUpLinks.Add(Space.RowColumn(cell / 9, cell % 9));
+				lightUpLinks.Add(Space.BlockDigit(cell >> HouseType.Block, digit));
+				lightUpLinks.Add(Space.RowDigit((cell >> HouseType.Row) - 9, digit));
+				lightUpLinks.Add(Space.ColumnDigit((cell >> HouseType.Column) - 18, digit));
+			}
+			result &= lightUpLinks;
+		}
+		return result;
+	}
 }
