f20ac3c980f20c3581671c27b5b732583a145287
Kyouyama Kazusa <kazusa0805@outlook.com>
2025-10-13 01:07:09 +0800

    Fix typo.

--- src/Sudoku.Analytics/SetTheory/PermutationCount.cs
+++ src/Sudoku.Analytics/SetTheory/PermutationCount.cs
@@ -2,11 +2,11 @@ namespace Sudoku.SetTheory;
 
 /// <summary>
 /// Represents a range of number of permutations found in a pattern.
-/// Instances of this type will be returned by method <see cref="PatternReasoner.GetAssignementsCount(in Pattern)"/>.
+/// Instances of this type will be returned by method <see cref="PatternReasoner.GetAssignmentsCount(in Pattern)"/>.
 /// </summary>
 /// <param name="Min">Indicates the minimum number of a permutation.</param>
 /// <param name="Max">Indicates the maximum number of a permutation.</param>
-/// <seealso cref="PatternReasoner.GetAssignementsCount(in Pattern)"/>
+/// <seealso cref="PatternReasoner.GetAssignmentsCount(in Pattern)"/>
 public readonly record struct AssignmentCountRange(int Min, int Max) :
 	IEqualityOperators<AssignmentCountRange, AssignmentCountRange, bool>
 {
