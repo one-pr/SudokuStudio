3d014af4b76537ab00ac6de28510f2b583a19794
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-10-15 15:04:26 +0800

    Update code.

---
+++ src/Sudoku.Analytics/SetTheory/PatternReasoner.Cached.cs
@@ -0,0 +1,140 @@
+namespace Sudoku.SetTheory;
+
+public partial class PatternReasoner
+{
+	/// <summary>
+	/// Represents equivalent implementation of the parent type, but an extra parameter <c>permutations</c> is required.
+	/// </summary>
+	private static class Cached
+	{
+		public static int GetEliminationRank(in Pattern pattern, Candidate candidate, ReadOnlySpan<Permutation> permutations)
+		{
+			ref readonly var links = ref pattern.Links;
+			var (maxOccupied, minOccupied) = (0, links.Count);
+			foreach (var permutation in permutations)
+			{
+				var lightupLinks = SpaceSet.Empty;
+				foreach (var assigned in permutation)
+				{
+					foreach (var link in assigned.Spaces)
+					{
+						if (links.Contains(link))
+						{
+							lightupLinks += link;
+						}
+					}
+				}
+
+				var occupied = lightupLinks.Count;
+				if (occupied >= maxOccupied)
+				{
+					maxOccupied = occupied;
+				}
+				if (occupied <= minOccupied)
+				{
+					minOccupied = occupied;
+				}
+			}
+			return maxOccupied - minOccupied;
+		}
+
+		public static AssignmentCountRange GetAssignmentsCount(in Pattern pattern, ReadOnlySpan<Permutation> permutations)
+		{
+			var (min, max) = (int.MaxValue, int.MinValue);
+			foreach (var permutation in permutations)
+			{
+				var count = permutation.Assignments.Length;
+				if (count <= min)
+				{
+					min = count;
+				}
+				if (count >= max)
+				{
+					max = count;
+				}
+			}
+			return (min, max) is ( >= 0, >= 0) ? new(min, max) : new();
+		}
+
+		public static ReadOnlySpan<Conclusion> GetConclusions(in Pattern pattern, ReadOnlySpan<Permutation> permutations)
+		{
+			ref readonly var grid = ref pattern.Grid;
+			ref readonly var fullMap = ref pattern.FullMap;
+			var candidatesMap = grid.CandidatesMap;
+
+			var result = ConclusionSet.Empty;
+			var i = 0;
+			foreach (var permutation in permutations)
+			{
+				var tempConclusions = ConclusionSet.Empty;
+				foreach (var candidate in permutation)
+				{
+					var cell = candidate / 9;
+					var digit = candidate % 9;
+					foreach (var c in PeersMap[cell] & candidatesMap[digit])
+					{
+						if (fullMap.Contains(c * 9 + digit))
+						{
+							tempConclusions.Add(new(Elimination, c, digit));
+						}
+					}
+					foreach (var d in (Mask)(grid.GetCandidates(cell) & ~(1 << digit)))
+					{
+						if (fullMap.Contains(cell * 9 + d))
+						{
+							tempConclusions.Add(new(Elimination, cell, d));
+						}
+					}
+					tempConclusions.Add(new(Assignment, cell, digit));
+				}
+
+				if (i++ == 0)
+				{
+					result |= tempConclusions;
+				}
+				else
+				{
+					result &= tempConclusions;
+				}
+			}
+			return result.AsSpan();
+		}
+
+		public static SpaceSet GetRank0Links(in Pattern pattern, ReadOnlySpan<Permutation> permutations)
+		{
+			var result = pattern.Links;
+			foreach (var permutation in permutations)
+			{
+				var lightUpLinks = SpaceSet.Empty;
+				foreach (var assigned in permutation)
+				{
+					lightUpLinks.AddRange(assigned.Spaces);
+				}
+				result &= lightUpLinks;
+			}
+			return result;
+		}
+
+		public static CandidateMap GetRank0Eliminations(in Pattern pattern, ReadOnlySpan<Permutation> permutations)
+		{
+			var conclusions = GetConclusions(pattern, permutations);
+			var result = CandidateMap.Empty;
+			foreach (var (type, candidate) in conclusions)
+			{
+				if (type != Elimination)
+				{
+					continue;
+				}
+
+				foreach (var link in GetRank0Links(pattern, permutations))
+				{
+					if (link.Contains(candidate))
+					{
+						result.Add(candidate);
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
--- src/Sudoku.Analytics/SetTheory/PatternReasoner.cs
+++ src/Sudoku.Analytics/SetTheory/PatternReasoner.cs
@@ -3,7 +3,7 @@ namespace Sudoku.SetTheory;
 /// <summary>
 /// Provides a list of members to get pattern detailed information that should be inferred.
 /// </summary>
-public static class PatternReasoner
+public static partial class PatternReasoner
 {
 	/// <summary>
 	/// Gets rank of specified elimination. The rank of elimination is defined as <c>n(links) - n(lightup_links)</c>.
@@ -12,35 +12,7 @@ public static class PatternReasoner
 	/// <param name="candidate">The candidate.</param>
 	/// <returns>The rank of elimination. -1 will be returned if candidate is not an eliminiation.</returns>
 	public static int GetEliminationRank(in Pattern pattern, Candidate candidate)
-	{
-		ref readonly var links = ref pattern.Links;
-		var (maxOccupied, minOccupied) = (0, links.Count);
-		foreach (var permutation in GetPermutations(pattern))
-		{
-			var lightupLinks = SpaceSet.Empty;
-			foreach (var assigned in permutation)
-			{
-				foreach (var link in assigned.Spaces)
-				{
-					if (links.Contains(link))
-					{
-						lightupLinks += link;
-					}
-				}
-			}
-
-			var occupied = lightupLinks.Count;
-			if (occupied >= maxOccupied)
-			{
-				maxOccupied = occupied;
-			}
-			if (occupied <= minOccupied)
-			{
-				minOccupied = occupied;
-			}
-		}
-		return maxOccupied - minOccupied;
-	}
+		=> Cached.GetEliminationRank(pattern, candidate, GetPermutations(pattern));
 
 	/// <summary>
 	/// <para>
@@ -58,22 +30,7 @@ public static class PatternReasoner
 	/// <returns>The permutation count value.</returns>
 	/// <seealso cref="AssignmentCountRange"/>
 	public static AssignmentCountRange GetAssignmentsCount(in Pattern pattern)
-	{
-		var (min, max) = (int.MaxValue, int.MinValue);
-		foreach (var permutation in GetPermutations(pattern))
-		{
-			var count = permutation.Assignments.Length;
-			if (count <= min)
-			{
-				min = count;
-			}
-			if (count >= max)
-			{
-				max = count;
-			}
-		}
-		return (min, max) is ( >= 0, >= 0) ? new(min, max) : new();
-	}
+		=> Cached.GetAssignmentsCount(pattern, GetPermutations(pattern));
 
 	/// <summary>
 	/// Try to find all possible permutations.
@@ -88,69 +45,14 @@ public static class PatternReasoner
 	/// <param name="pattern">The pattern.</param>
 	/// <returns>All conclusions.</returns>
 	public static ReadOnlySpan<Conclusion> GetConclusions(in Pattern pattern)
-	{
-		ref readonly var grid = ref pattern.Grid;
-		ref readonly var fullMap = ref pattern.FullMap;
-		var candidatesMap = grid.CandidatesMap;
-
-		var result = ConclusionSet.Empty;
-		var i = 0;
-		foreach (var permutation in GetPermutations(pattern))
-		{
-			var tempConclusions = ConclusionSet.Empty;
-			foreach (var candidate in permutation)
-			{
-				var cell = candidate / 9;
-				var digit = candidate % 9;
-				foreach (var c in PeersMap[cell] & candidatesMap[digit])
-				{
-					if (fullMap.Contains(c * 9 + digit))
-					{
-						tempConclusions.Add(new(Elimination, c, digit));
-					}
-				}
-				foreach (var d in (Mask)(grid.GetCandidates(cell) & ~(1 << digit)))
-				{
-					if (fullMap.Contains(cell * 9 + d))
-					{
-						tempConclusions.Add(new(Elimination, cell, d));
-					}
-				}
-				tempConclusions.Add(new(Assignment, cell, digit));
-			}
-
-			if (i++ == 0)
-			{
-				result |= tempConclusions;
-			}
-			else
-			{
-				result &= tempConclusions;
-			}
-		}
-		return result.AsSpan();
-	}
+		=> Cached.GetConclusions(pattern, GetPermutations(pattern));
 
 	/// <summary>
 	/// Gets all rank-0 links.
 	/// </summary>
 	/// <param name="pattern">The pattern.</param>
 	/// <returns>All rank-0 links.</returns>
-	public static SpaceSet GetRank0Links(in Pattern pattern)
-	{
-		var permutations = GetPermutations(pattern);
-		var result = pattern.Links;
-		foreach (var permutation in permutations)
-		{
-			var lightUpLinks = SpaceSet.Empty;
-			foreach (var assigned in permutation)
-			{
-				lightUpLinks.AddRange(assigned.Spaces);
-			}
-			result &= lightUpLinks;
-		}
-		return result;
-	}
+	public static SpaceSet GetRank0Links(in Pattern pattern) => Cached.GetRank0Links(pattern, GetPermutations(pattern));
 
 	/// <summary>
 	/// Gets all rank-0 eliminations.
@@ -158,25 +60,5 @@ public static class PatternReasoner
 	/// <param name="pattern">The pattern.</param>
 	/// <returns>All rank-0 eliminations.</returns>
 	public static CandidateMap GetRank0Eliminations(in Pattern pattern)
-	{
-		var conclusions = GetConclusions(pattern);
-		var result = CandidateMap.Empty;
-		foreach (var (type, candidate) in conclusions)
-		{
-			if (type != Elimination)
-			{
-				continue;
-			}
-
-			// TODO: construct a cache. Unnecessary to calculate permutations twice.
-			foreach (var link in GetRank0Links(pattern))
-			{
-				if (link.Contains(candidate))
-				{
-					result.Add(candidate);
-				}
-			}
-		}
-		return result;
-	}
+		=> Cached.GetRank0Eliminations(pattern, GetPermutations(pattern));
 }
