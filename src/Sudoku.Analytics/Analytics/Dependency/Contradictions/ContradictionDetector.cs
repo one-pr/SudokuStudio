7b7dedd3f27c4ea93c3ce1bb5fe3f94eca8e81a6
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-11-13 15:19:49 +0800

    Update code.

--- src/Sudoku.Analytics/Analytics/Dependency/Contradictions/ContradictionDetector.cs
+++ src/Sudoku.Analytics/Analytics/Dependency/Contradictions/ContradictionDetector.cs
@@ -1,42 +1,243 @@
 namespace Sudoku.Analytics.Dependency.Contradictions;
 
 /// <summary>
-/// Represents a type that can find for conflict that will be used in complex patterns.
+/// Represents a type that can find for contradiction that will be used in complex patterns.
 /// </summary>
-public static partial class ContradictionDetector
+public static class ContradictionDetector
 {
 	/// <summary>
-	/// The global method to check for complex contradiction.
+	/// Do try and error logic (T&amp;E) and find for all candidates that causes contradiction.
 	/// </summary>
 	/// <param name="grid">The grid.</param>
-	/// <param name="includesGroupedNodes">Indicates whether grouped nodes should also be checked.</param>
-	/// <returns>A list of found conflict.</returns>
-	public static ReadOnlySpan<Cause> Check(in Grid grid, bool includesGroupedNodes)
+	/// <param name="includesGroupedNodes">Indicates whether the searching method will includes grouped nodes.</param>
+	/// <returns>All candidates making contradictions.</returns>
+	public static ReadOnlySpan<Candidate> TryAndError(in Grid grid, bool includesGroupedNodes)
 	{
-		var result = new List<Cause>();
-		if (!grid.IsValid)
+		var result = new List<Candidate>();
+		for (var cell = 0; cell < 81; cell++)
 		{
-			return result.AsSpan();
-		}
-
-		var solution = grid.SolutionGrid;
-
-		// Iterate on each empty cell.
-		foreach (var cell in grid.EmptyCells)
-		{
-			// Iterate on each wrong digit.
-			foreach (var digit in (Mask)(grid.GetCandidates(cell) & ~(1 << solution.GetDigit(cell))))
+			if (grid.GetState(cell) != CellState.Empty)
 			{
-				// Do a fast check, to know whether the candidate is worth to be an elimination or not.
-				if (DoFastTryAndError(grid, cell, digit, includesGroupedNodes)
-					&& LeadsToEmpty(grid, cell, digit, includesGroupedNodes, out var lastNode, out var cause))
+				continue;
+			}
+
+			foreach (var digit in grid.GetCandidates(cell))
+			{
+				if (tryAndError(grid, cell, digit, includesGroupedNodes))
 				{
-					result.Add(new(cell * 9 + digit, lastNode, cause));
+					result.Add(cell * 9 + digit);
 				}
 			}
 		}
-
-		// All candidates are checked.
 		return result.AsSpan();
+
+
+		static bool tryAndError(in Grid grid, Cell cell, Digit digit, bool includesGroupedNodes)
+		{
+			// Fast check. Just assign it and find for conclusions.
+			var firstAssignment = new DependencyAssignment(cell * 9 + digit);
+			var tempGrid = grid;
+			Update(ref tempGrid, firstAssignment);
+
+			bool isChanged;
+			do
+			{
+				isChanged = false;
+
+				if (tryFindConflict(tempGrid))
+				{
+					return true;
+				}
+
+				// Find for the next conclusion.
+				var collector = new HashSet<DependencyAssignment>();
+
+				// Collect for valid next steps.
+				FindHiddenSingle(tempGrid, collector);
+				FindNakedSingle(tempGrid, collector);
+				if (includesGroupedNodes)
+				{
+					FindLockedCandidates(tempGrid, collector);
+				}
+
+				if (collector.Count != 0)
+				{
+					isChanged = true;
+					foreach (var assignment in collector)
+					{
+						Update(ref tempGrid, assignment);
+					}
+				}
+			} while (isChanged);
+
+			// No contradictions found.
+			return false;
+		}
+
+		static bool tryFindConflict(in Grid grid)
+		{
+			// Check cell.
+			foreach (var cell in grid.EmptyCells)
+			{
+				if (grid.GetCandidates(cell) == 0)
+				{
+					return true;
+				}
+			}
+
+			// Check house.
+			for (var digit = 0; digit < 9; digit++)
+			{
+				for (var house = 0; house < 27; house++)
+				{
+					var count = 0;
+					foreach (var cell in HousesMap[house])
+					{
+						var cellState = grid.GetState(cell);
+						if (grid.GetDigit(cell) == digit)
+						{
+							goto NextHouse;
+						}
+						if (grid.Exists(cell, digit) is true)
+						{
+							count++;
+						}
+					}
+					if (count == 0)
+					{
+						return true;
+					}
+
+				NextHouse:;
+				}
+			}
+			return false;
+		}
 	}
+
+	/// <summary>
+	/// Create a new grid that applies the specified assignment.
+	/// </summary>
+	/// <param name="grid">The grid to be updated.</param>
+	/// <param name="assignment">The assignment.</param>
+	private static void Update(scoped ref Grid grid, DependencyAssignment assignment)
+	{
+		_ = assignment is (var digit, { PeerIntersection: var peerCells } cells);
+		foreach (var peerCell in peerCells)
+		{
+			ref var mask = ref grid[peerCell];
+			if (MaskToCellState(mask) == CellState.Empty && (mask >> digit & 1) != 0)
+			{
+				mask &= (Mask)~(1 << digit);
+			}
+		}
+
+		// Set digit for the current cell.
+		if (cells is [var cell])
+		{
+			// Updates mask of the target cell.
+			grid[cell] = (Mask)(GetHeaderBits(in grid, cell) | Grid.ModifiableMask | 1 << digit);
+		}
+	}
+
+	/// <summary>
+	/// Find for hidden singles, and collect them into <paramref name="result"/>.
+	/// </summary>
+	/// <param name="grid">The grid.</param>
+	/// <param name="result">The result.</param>
+	private static void FindHiddenSingle(in Grid grid, HashSet<DependencyAssignment> result)
+	{
+		// Iterate on each digit.
+		for (var digit = 0; digit < 9; digit++)
+		{
+			// Iterate on each house.
+			for (var house = 0; house < 27; house++)
+			{
+				var count = 0;
+				var firstFoundCell = -1;
+				foreach (var cell in HousesMap[house])
+				{
+					var cellState = grid.GetState(cell);
+					if (grid.GetDigit(cell) == digit)
+					{
+						goto NextHouse;
+					}
+					if (grid.Exists(cell, digit) is true)
+					{
+						firstFoundCell = cell;
+						if (++count >= 2)
+						{
+							break;
+						}
+					}
+				}
+
+				if (count == 1)
+				{
+					result.Add(new(firstFoundCell * 9 + digit));
+				}
+
+			NextHouse:;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Find for naked singles, and collect them into <paramref name="result"/>.
+	/// </summary>
+	/// <param name="grid">The grid.</param>
+	/// <param name="result">The result.</param>
+	private static void FindNakedSingle(in Grid grid, HashSet<DependencyAssignment> result)
+	{
+		foreach (var cell in grid.EmptyCells)
+		{
+			var mask = grid.GetCandidates(cell);
+			if (BitOperations.IsPow2((uint)mask))
+			{
+				result.Add(new(cell * 9 + BitOperations.Log2((uint)mask)));
+			}
+		}
+	}
+
+	/// <summary>
+	/// Find for locked candidates, and collect them into <paramref name="result"/>.
+	/// </summary>
+	/// <param name="grid">The grid.</param>
+	/// <param name="result">The result.</param>
+	private static void FindLockedCandidates(in Grid grid, HashSet<DependencyAssignment> result)
+	{
+		var emptyCells = grid.EmptyCells;
+		var candidatesMap = grid.CandidatesMap;
+		foreach (var ((baseSet, coverSet), (a, b, c, _)) in Miniline.Map)
+		{
+			// Check whether the locked candidates pattern can be formed or not.
+			if (!LockedCandidates.IsLockedCandidates(grid, a, b, c, emptyCells, out var digitsMaskFormingLockedCandidates))
+			{
+				continue;
+			}
+
+			// Now iterate on the mask to get all digits.
+			foreach (var digit in digitsMaskFormingLockedCandidates)
+			{
+				ref readonly var map = ref candidatesMap[digit];
+
+				// Check whether the digit contains any eliminations.
+				var intersection = c & map;
+				if (intersection.Count >= 2)
+				{
+					// a & map => Cells in lines are not empty => pointing eliminations.
+					result.Add(new(digit, intersection));
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// An unsafe entry to method <c>GetHeaderBits</c> defined in type <see cref="Grid"/>.
+	/// </summary>
+	/// <param name="grid">The grid.</param>
+	/// <param name="cell">The cell.</param>
+	/// <returns>The mask of mask bits, unshifted.</returns>
+	[UnsafeAccessor(UnsafeAccessorKind.Method, Name = "GetHeaderBits")]
+	private static extern Mask GetHeaderBits(ref readonly Grid grid, Cell cell);
 }
